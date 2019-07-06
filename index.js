4f94bd02cf29b4d94ac9d135092ec0308a670cd7
Evgeniy_Marushev <emarushev@eshe.studio>
2019-07-06 22:13:53 +0300

    added streams

--- index.js
+++ index.js
@@ -2,13 +2,30 @@
 "use strict";
 const fs = require('fs');
 const _ = require('lodash');
+const util = require('util');
+const path = require('path');
+const getStdIn = require('get-stdin');
+const Transform = require('stream').Transform;
 process.stdout.write(process.argv + '\n');
 process.stderr.write('error\n');
-const { file } = require('minimist')(process.argv.slice(2), {
-    string: ['file']
+const { file, in: _in } = require('minimist')(process.argv.slice(2), {
+    string: ['file'],
+    boolean: ['in']
 });
 if (file) {
-    fs.readFile(file, (err, data) => {
-        process.stdout.write(data.toString())
-    })
+    const stream = fs.createReadStream(path.resolve(file));
+    // process.stdout.write(stream.toString());
+    // console.log(stream);
+    workWithStream(stream);
+} else if (_in) {
+    workWithStream(process.stdin);
 }
+
+function workWithStream(inStream) {
+    const outStream = inStream;
+    outStream.pipe(process.stdout);
+
+}
+
+
+// console.log(process.env)
